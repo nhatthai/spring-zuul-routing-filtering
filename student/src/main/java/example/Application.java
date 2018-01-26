a22e6f5f59ff3054ae625a29aa0ce9a90157ce12
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2018-01-26 17:37:01 +0700

    Update method

--- student/src/main/java/example/Application.java
+++ student/src/main/java/example/Application.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @SpringBootApplication
 public class Application {
 
-    @RequestMapping(value = "/available")
+    @GetMapping(value = "/available")
     public String available() {
         return "Spring in Action";
     }
